0fd548a798f4b79e94570b5c84bec5f598e3133d
Shakespeares1 <adeavilap1@unicartagena.edu.co>
2025-11-30 00:19:48 -0500

    Add files via upload

---
+++ SCRIPT/script_eventos_congreso.sql
@@ -0,0 +1,144 @@
+CREATE DATABASE  IF NOT EXISTS `eventos_congreso` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `eventos_congreso`;
+-- MySQL dump 10.13  Distrib 8.0.44, for Win64 (x86_64)
+--
+-- Host: localhost    Database: eventos_congreso
+-- ------------------------------------------------------
+-- Server version	8.0.44
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `congresista`
+--
+
+DROP TABLE IF EXISTS `congresista`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `congresista` (
+  `id_congresista` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(60) NOT NULL,
+  `apellido` varchar(60) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `pais` varchar(60) DEFAULT NULL,
+  `institucion` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id_congresista`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `congresista`
+--
+
+LOCK TABLES `congresista` WRITE;
+/*!40000 ALTER TABLE `congresista` DISABLE KEYS */;
+INSERT INTO `congresista` VALUES (1,'Ana','Pérez','ana.perez@example.com','Colombia','Universidad de Cartagena'),(2,'Luis','Gómez','luis.gomez@example.com','México','UNAM'),(3,'María','Rojas','maria.rojas@example.com','Perú','PUCP');
+/*!40000 ALTER TABLE `congresista` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sala`
+--
+
+DROP TABLE IF EXISTS `sala`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sala` (
+  `id_sala` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(60) NOT NULL,
+  `capacidad` int NOT NULL,
+  PRIMARY KEY (`id_sala`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sala`
+--
+
+LOCK TABLES `sala` WRITE;
+/*!40000 ALTER TABLE `sala` DISABLE KEYS */;
+INSERT INTO `sala` VALUES (1,'Auditorio Principal',200),(2,'Sala 101',50),(3,'Sala 102',50);
+/*!40000 ALTER TABLE `sala` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sesion`
+--
+
+DROP TABLE IF EXISTS `sesion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sesion` (
+  `id_sesion` int NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(100) NOT NULL,
+  `fecha` date NOT NULL,
+  `hora_inicio` time NOT NULL,
+  `hora_fin` time NOT NULL,
+  `id_sala` int NOT NULL,
+  PRIMARY KEY (`id_sesion`),
+  KEY `fk_sesion_sala` (`id_sala`),
+  CONSTRAINT `fk_sesion_sala` FOREIGN KEY (`id_sala`) REFERENCES `sala` (`id_sala`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sesion`
+--
+
+LOCK TABLES `sesion` WRITE;
+/*!40000 ALTER TABLE `sesion` DISABLE KEYS */;
+INSERT INTO `sesion` VALUES (1,'Apertura del Congreso','2025-11-30','08:00:00','09:00:00',1),(2,'Inteligencia Artificial','2025-11-30','09:30:00','11:00:00',2),(3,'Desarrollo de Software','2025-11-30','11:30:00','13:00:00',3);
+/*!40000 ALTER TABLE `sesion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `trabajo`
+--
+
+DROP TABLE IF EXISTS `trabajo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `trabajo` (
+  `id_trabajo` int NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(120) NOT NULL,
+  `area_tematica` varchar(80) DEFAULT NULL,
+  `id_congresista` int NOT NULL,
+  `id_sesion` int NOT NULL,
+  PRIMARY KEY (`id_trabajo`),
+  KEY `fk_trabajo_congresista` (`id_congresista`),
+  KEY `fk_trabajo_sesion` (`id_sesion`),
+  CONSTRAINT `fk_trabajo_congresista` FOREIGN KEY (`id_congresista`) REFERENCES `congresista` (`id_congresista`),
+  CONSTRAINT `fk_trabajo_sesion` FOREIGN KEY (`id_sesion`) REFERENCES `sesion` (`id_sesion`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `trabajo`
+--
+
+LOCK TABLES `trabajo` WRITE;
+/*!40000 ALTER TABLE `trabajo` DISABLE KEYS */;
+INSERT INTO `trabajo` VALUES (1,'Uso de IA en Educación','Inteligencia Artificial',1,2),(2,'Arquitecturas Hexagonales','Desarrollo de Software',2,3),(3,'Bases de datos distribuidas','Bases de Datos',3,3);
+/*!40000 ALTER TABLE `trabajo` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-11-29 16:28:29
